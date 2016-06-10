b17ab30747885a80dfedc3f33bc80d3bd0a9455f
wsy <278876627@qq.com>
2016-06-10 10:25:11 +0800

    重新安装entityframework

--- Libraries/Petite.EntityFramework/Repository/PetiteRepository.cs
+++ Libraries/Petite.EntityFramework/Repository/PetiteRepository.cs
@@ -14,8 +14,6 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Validation;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Petite.Core;
 
 namespace Petite.Data.Repository
