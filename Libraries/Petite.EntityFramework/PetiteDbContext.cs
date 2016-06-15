0712fa78352091e5d0315058488fc29242f47723
wsy <278876627@qq.com>
2016-06-15 14:48:55 +0800

    添加UnitOfWork

--- Libraries/Petite.EntityFramework/PetiteDbContext.cs
+++ Libraries/Petite.EntityFramework/PetiteDbContext.cs
@@ -54,105 +54,10 @@ namespace Petite.Data
             base.OnModelCreating(modelBuilder);
         }
               
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Create database script
-        /// </summary>
-        /// <returns>SQL to generate database</returns>
-        public string CreateDatabaseScript()
-        {
-            return ((IObjectContextAdapter)this).ObjectContext.CreateDatabaseScript();
-        }
-
-        /// <summary>
-        /// Get DbSet
-        /// </summary>
-        /// <typeparam name="TEntity">Entity type</typeparam>
-        /// <returns>DbSet</returns>
-        public IDbSet<TEntity> Set<TEntity,TPrimaryKey>() where TEntity : class,IEntity<TPrimaryKey>
-        {
-            return base.Set<TEntity>();
-        }
-
-        /// <summary>
-        /// Creates a raw SQL query that will return elements of the given generic type.  The type can be any type that has properties that match the names of the columns returned from the query, or can be a simple primitive type. The type does not have to be an entity type. The results of this query are never tracked by the context even if the type of object returned is an entity type.
-        /// </summary>
-        /// <typeparam name="TElement">The type of object returned by the query.</typeparam>
-        /// <param name="sql">The SQL query string.</param>
-        /// <param name="parameters">The parameters to apply to the SQL query string.</param>
-        /// <returns>Result</returns>
-        public IEnumerable<TElement> SqlQuery<TElement>(string sql, params object[] parameters)
-        {
-            return this.Database.SqlQuery<TElement>(sql, parameters);
-        }
-
-        /// <summary>
-        /// Executes the given DDL/DML command against the database.
-        /// </summary>
-        /// <param name="sql">The command string</param>
-        /// <param name="doNotEnsureTransaction">false - the transaction creation is not ensured; true - the transaction creation is ensured.</param>
-        /// <param name="timeout">Timeout value, in seconds. A null value indicates that the default value of the underlying provider will be used</param>
-        /// <param name="parameters">The parameters to apply to the command string.</param>
-        /// <returns>The result returned by the database after executing the command.</returns>
-        public int ExecuteSqlCommand(string sql, bool doNotEnsureTransaction = false, int? timeout = null, params object[] parameters)
-        {
-            int? previousTimeout = null;
-            if (timeout.HasValue)
-            {
-                //store previous timeout
-                previousTimeout = ((IObjectContextAdapter)this).ObjectContext.CommandTimeout;
-                ((IObjectContextAdapter)this).ObjectContext.CommandTimeout = timeout;
-            }
-
-            var transactionalBehavior = doNotEnsureTransaction
-                ? TransactionalBehavior.DoNotEnsureTransaction
-                : TransactionalBehavior.EnsureTransaction;
-            var result = this.Database.ExecuteSqlCommand(transactionalBehavior, sql, parameters);
-
-            if (timeout.HasValue)
-            {
-                //Set previous timeout back
-                ((IObjectContextAdapter)this).ObjectContext.CommandTimeout = previousTimeout;
-            }
-
-            //return result
-            return result;
-        }
-
-        /// <summary>
-        /// Detach an entity
-        /// </summary>
-        /// <param name="entity">Entity</param>
-        public void Detach(object entity)
-        {
-            if (entity == null)
-                throw new ArgumentNullException("entity");
-
-            ((IObjectContextAdapter)this).ObjectContext.Detach(entity);
-        }
-
-        #endregion
+        #endregion        
 
         #region Properties
-
-        /// <summary>
-        /// Gets or sets a value indicating whether proxy creation setting is enabled (used in EF)
-        /// </summary>
-        public virtual bool ProxyCreationEnabled
-        {
-            get
-            {
-                return this.Configuration.ProxyCreationEnabled;
-            }
-            set
-            {
-                this.Configuration.ProxyCreationEnabled = value;
-            }
-        }
-
+        
         /// <summary>
         /// Gets or sets a value indicating whether auto detect changes setting is enabled (used in EF)
         /// </summary>
---
+++ Libraries/Petite.EntityFramework/Uow/UnitOfWork.cs
@@ -0,0 +1,87 @@
+﻿//======================================================================  
+//  
+//        Copyright (C) 2016 哈分享网      
+//        All rights reserved    
+//        Filename :UnitOfWork 
+//        Description :    
+//        Created by Wsy at 2016/6/13 17:48:47
+//        http://www.hafenxiang.com 
+//  
+//======================================================================  
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Petite.Data.Uow
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public bool TransactionEnabled { get; set; }
+
+        public int ExecuteSqlCommand(TransactionalBehavior transactionalBehavior, string sql, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable ExecuteStoredProcedureList(string commandText, params object[] parameters)
+        {
+            //add parameters to command
+            if (parameters != null && parameters.Length > 0)
+            {
+                for (int i = 0; i <= parameters.Length - 1; i++)
+                {
+                    var p = parameters[i] as DbParameter;
+                    if (p == null)
+                        throw new Exception("Not support parameter type");
+
+                    commandText += i == 0 ? " " : ", ";
+
+                    commandText += "@" + p.ParameterName;
+                    if (p.Direction == ParameterDirection.InputOutput || p.Direction == ParameterDirection.Output)
+                    {
+                        //output parameter
+                        commandText += " output";
+                    }
+                }
+            }
+
+            var result = this.Database.SqlQuery<TEntity>(commandText, parameters).ToList();
+            
+            bool acd = this.Configuration.AutoDetectChangesEnabled;
+            try
+            {
+                this.Configuration.AutoDetectChangesEnabled = false;
+
+                for (int i = 0; i < result.Count; i++)
+                    result[i] = AttachEntityToContext(result[i]);
+            }
+            finally
+            {
+                this.Configuration.AutoDetectChangesEnabled = acd;
+            }
+
+            return result;
+        }
+
+        public int SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable SqlQuery(Type elementType, string sql, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TElement> SqlQuery<TElement>(string sql, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
