431057cb2d528b7ad6b72faaa004ffedaa245b8c
wsy <278876627@qq.com>
2016-06-20 18:20:20 +0800

    添加UnitOfWorkOptions

--- Libraries/Petite.EntityFramework/Uow/UnitOfWorkOptions.cs
+++ Libraries/Petite.EntityFramework/Uow/UnitOfWorkOptions.cs
@@ -2,35 +2,24 @@
 //  
 //        Copyright (C) 2016 哈分享网      
 //        All rights reserved    
-//        Filename :UnitOfWork 
+//        Filename :UnitOfWorkOptions 
 //        Description :    
-//        Created by Wsy at 2016/6/13 17:48:47
+//        Created by Wsy at 2016/6/20 14:14:28
 //        http://www.hafenxiang.com 
 //  
 //======================================================================  
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Petite.EntityFramework.Uow
 {
-    public class UnitOfWork : IUnitOfWork
+    public class UnitOfWorkOptions
     {
-        public bool TransactionEnabled { get; set; }
-
-        public int Commit()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task CommitAsync()
-        {
-            throw new NotImplementedException();
-        }
+        public TransactionScopeOption
     }
 }
